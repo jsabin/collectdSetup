af7acd21b10f1db56771539d615b6cb0485ade0f
Jeff Sabin <jsabin@proofpoint.com>
2014-12-02 14:24:11 -0700

    Created script

--- installCollectd.sh
+++ installCollectd.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
 
+# Instructions: Run as sudo or root
+
+cwd=$(pwd)
+
+# Get zip file
+wget https://raw.githubusercontent.com/jsabin/collectdSetup/master/collectd.gzip
+
+# Extract zip
+pushd /
+tar xvfz $cwd/collectd.gzip
+chmod -R root /opt/proofpoint/collectd
+chgrp -R root /opt/proofpoint/collectd
+
+# Copy config file
+mkdir /etc/collectd
+cp /opt/proofpoint/collectd/distribute/collectd.conf /etc/collectd
+
+# Add links
+sudo ln --symbolic /opt/proofpoint/collectd/sbin/collectd /usr/sbin/collectd
+sudo ln --symbolic /opt/proofpoint/collectd/sbin/collectdmon /usr/sbin/collectdmon
+
+# Copy startup script
+cp /opt/proofpoint/collectd/distribute/collectd /etc/init.d
+update-rc.d collectd defaults
+
+# Start collectd
+/etc/init.d/collectd start
+
+popd
+rm collectd.gzip
+
+
